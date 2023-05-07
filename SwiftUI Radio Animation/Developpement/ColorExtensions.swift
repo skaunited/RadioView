6bff4784ae46286ecd6860905bbe3037d8bbc8b3
Timothé Micheneau <t.micheneau@workflow-dev.fr>
2023-05-07 16:33:38 +0200

    first commit

---
+++ SwiftUI Radio Animation/Developpement/ColorExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtensions.swift
+//  SwiftUI Radio Animation
+//
+//  Created by Work Flow Dev on 07/05/2023.
+//
+
+import Foundation
---
+++ SwiftUI Radio Animation/NumeoRadioView.swift
@@ -0,0 +1,20 @@
+//
+//  NumeoRadioView.swift
+//  SwiftUI Radio Animation
+//
+//  Created by Work Flow Dev on 07/05/2023.
+//
+
+import SwiftUI
+
+struct NumeoRadioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NumeoRadioView_Previews: PreviewProvider {
+    static var previews: some View {
+        NumeoRadioView()
+    }
+}
