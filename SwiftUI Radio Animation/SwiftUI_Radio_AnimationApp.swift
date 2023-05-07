3fbae903092c7d66b6f660934e6003127a2626e6
Timothé Micheneau <t.micheneau@workflow-dev.fr>
2023-05-07 10:54:31 +0200

    Initial Commit

---
+++ SwiftUI Radio Animation/SwiftUI_Radio_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Radio_AnimationApp.swift
+//  SwiftUI Radio Animation
+//
+//  Created by Work Flow Dev on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Radio_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Radio AnimationUITests/SwiftUI_Radio_AnimationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_Radio_AnimationUITestsLaunchTests.swift
+//  SwiftUI Radio AnimationUITests
+//
+//  Created by Work Flow Dev on 07/05/2023.
+//
+
+import XCTest
+
+final class SwiftUI_Radio_AnimationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
